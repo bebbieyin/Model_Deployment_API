e903d153a58ebb4601d3a435cdaad3fabc46abab
Your Name <bebbieyin.n@gmail.com>
2023-01-18 01:42:45 +0800

    'updates'

--- call.py
+++ call.py
@@ -1,7 +1,7 @@
 import requests
 
 image_path = "image_classification/test_imgs/banana.jpeg"
-url = 'http://localhost:5000/api'
+url = ' http://172.31.6.218:8000/api'
 
 
 r = requests.post(url, files={'image': open(image_path, "rb")})
